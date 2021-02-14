dab85664d581902faaf75d3e70daa278f0a57aa5
Konstas Marmatakis <marmako@gmail.com>
2021-02-14 02:18:43 +0200

    Commit 202102140218

--- pages/newbook.php
+++ pages/newbook.php
@@ -41,44 +41,71 @@
     */
     ?>
       <?php include '../dist/php/scripts.php'; ?>
-    <div>
-      <form action="" method="post"><br>
-        <label for="isbn">ISBN:</label>
-        <input type="text" id="isbn" name="isbn"><br>
-        <label for="isbn10">ISBN 10:</label>
-        <input type="text" id="isbn10" name="isbn10"><br>
-        <label for="title">Τίτλος:</label>
-        <input type="text" id="title" name="title"><br>
-        <label for="publisher_id">Εκδότης:</label>
-        <select id="publisher_id" name="publisher_id">
-        <?php
-          $publishers = get_publishers();
-          foreach ($publishers as $p){
-            echo '<option value="'.$p['publisher_id'].'">'.$p['name'].'</option>';
-          }
-        ?>
-        </select><br>
-        <label for="year">Έτος έκδοσης:</label>
-        <input type="text" id="year" name="year"><br>
-        <label for="pages">Σελίδες:</label>
-        <input type="text" id="pages" name="pages"><br>
-        <label for="back_page">Οπισθόφυλλο - Περιγραφή:</label>
-        <textarea id="back_page" name="back_page" rows="8" cols="50">
-        </textarea><br>
-        <label for="category_id">Κατηγορία:</label>
-        <select name="category_id" id="category_id">
-        <?php
-          $categories = get_categories();
-          foreach ($categories as $c){
-            echo '<option value="'.$c['category_id'].'">'.$c['description'].'</option>';
+    <div class="container">
+    <h2>Εισαγωγή Βιβλίου</h2>
+      <form action="" method="post">
+        <div class="row">
+          <div class="col-25"><label for="isbn">ISBN:</label></div>
+          <div class="col-75"><input type="text" id="isbn" name="isbn" placeholder="Καταχωρείστε το ISBN  του βιβλίου."></div>
+        </div>
+        <div class="row">
+          <div class="col-25"><label for="isbn10">ISBN 10:</label></div>
+          <div class="col-75"><input type="text" id="isbn10" name="isbn10"></div>
+        </div>
+        <div class="row">
+          <div class="col-25"><label for="title">Τίτλος:</label></div>
+          <div class="col-75"><input type="text" id="title" name="title"></div>
+        </div>
+        <div class="row">
+          <div class="col-25"><label for="publisher_id">Εκδότης:</label></div>
+          <div class="col-75">
+            <select id="publisher_id" name="publisher_id">
+            <?php
+              $publishers = get_publishers();
+              foreach ($publishers as $p){
+                echo '<option value="'.$p['publisher_id'].'">'.$p['name'].'</option>';
+              }
+            ?>
+            </select>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-25"><label for="year">Έτος έκδοσης:</label></div>
+          <div class="col-75"><input type="text" id="year" name="year"></div>
+        </div>
+        <div class="row">
+          <div class="col-25"><label for="pages">Σελίδες:</label></div>
+          <div class="col-75"><input type="text" id="pages" name="pages"></div>
+        </div>
+        <div class="row">
+          <div class="col-25"><label for="back_page">Οπισθόφυλλο - Περιγραφή:</label></div>
+          <div class="col-75">
+            <textarea id="back_page" name="back_page" rows="8" cols="50">
+            </textarea>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-25"><label for="category_id">Κατηγορία:</label></div>
+          <div class="col-75">
+            <select name="category_id" id="category_id">
+            <?php
+              $categories = get_categories();
+              foreach ($categories as $c){
+                echo '<option value="'.$c['category_id'].'">'.$c['description'].'</option>';
             
-          }
-        ?>
-        </select><br>
-        <label for="translated">Μεταφρασμένο:</label>
-        <input type="checkbox" name="translated" id="translated" value="1"><br>
-        <label for="translator_id">Μεταφραστής:</label>
-        <select name="translator_id" id="translator_id">
+              }
+            ?>
+            </select>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-25"><label for="translated">Μεταφρασμένο:</label></div>
+          <div class="col-75"><input type="checkbox" name="translated" id="translated" value="1"></div>
+        </div>
+        <div class="row">
+          <div class="col-25"><label for="translator_id">Μεταφραστής:</label></div>
+          <div class="col-75"><select name="translator_id" id="translator_id">
+        </div>
         <?php
           $translators = get_translators();
           foreach ($translators as $t){
@@ -86,31 +113,48 @@
             
           }
         ?>
-        </select><br>
-        <label for="author_id">Συγγραφέας:</label>
-        <select id="author_id" name="author_id">
-          <?php
-            $authors = get_authors();
-            foreach ($authors as $a){ 
-              echo '<option value="'.$a['author_id'].'">'.$a['fullname'].'</option>';
-            }
-          ?>
-        </select><br>
-        <label for="eidos_grafis_id">Είδος Γραφής:</label>
-        <select id="eidos_grafis_id" name="eidos_grafis_id">
-          <?php
-            $eidosgrafis = get_eidosgrafis();
-            foreach ($eidosgrafis as $eg){ 
-              echo '<option value="'.$eg['eidos_grafis_id'].'">'.$eg['description'].'</option>';
-            }
-          ?>
-        </select><br>
-        <label for="copies_standard">Αντίτυπα:</label>
-        <input type="text" name="copies_standard" id="copies_standard"><br>
-        <label for="copies_avail">Διαθέσιμα αντίγραφα:</label>
-        <input type="text" name="copies_avail" id="copies_avail"><br>
-        <label for="in_stock">Διαθέσιμο:</label>
-        <input type="checkbox" name="in_stock" id="in_stock" value="1"><br>
+        </select></div>
+        <div class="row">
+          <div class="col-25"><label for="author_id">Συγγραφέας:</label></div>
+          <div class="col-75">
+            <select id="author_id" name="author_id">
+              <?php
+                $authors = get_authors();
+                foreach ($authors as $a){
+                  echo '<option value="'.$a['author_id'].'">'.$a['fullname'].'</option>';
+                }
+              ?>
+            </select>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-25"><label for="eidos_grafis_id">Είδος Γραφής:</label></div>
+          <div class="col-75">
+            <select id="eidos_grafis_id" name="eidos_grafis_id">
+              <?php
+                $eidosgrafis = get_eidosgrafis();
+                foreach ($eidosgrafis as $eg){
+                  echo '<option value="'.$eg['eidos_grafis_id'].'">'.$eg['description'].'</option>';
+                }
+              ?>
+            </select>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-25"><label for="copies_standard">Αντίτυπα:</label></div>
+          <div class="col-75"><input type="text" name="copies_standard" id="copies_standard"></div>
+        </div>
+        <div class="row">
+          <div class="col-25"><label for="copies_avail">Διαθέσιμα αντίγραφα:</label></div>
+          <div class="col-75"><input type="text" name="copies_avail" id="copies_avail"></div>
+        </div>
+        <div class="row">
+          <div class="col-25"><label for="in_stock">Διαθέσιμο:</label></div>
+          <div class="col-75"><input type="checkbox" name="in_stock" id="in_stock" value="1"></div>
+        </div>
+        <div class="row">
+      <input type="submit" value="Καταχώρηση">
+    </div>
       </form>
     </div>  
     <!-- Script section -->
