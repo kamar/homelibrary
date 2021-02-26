f4681a2344cfbf1f6ff5007e36da108eb52d3e87
Konstas Marmatakis <marmako@gmail.com>
2021-02-26 14:26:00 +0200

    Commit 202102261425

--- app/php/connect.php
+++ app/php/connect.php
@@ -13,4 +13,18 @@ function con_homelibrary(){
     return $con;
 }
 
+
+functionn pdo_homelibrary(){
+    $host = "localhost"; 
+    $user = "km"; 
+    $pass = "j@nuoyla_01"; 
+    $db = "homelibrary";
+
+    try {
+        $con = new PDO("pgsql:dbname=$db;host=$host;user=$user;password=$pass")
+    } catch (PDOException $e) {
+        echo 'Connection failed: ' . $e->getMessage();
+    }
+    return $con;
+}
 ?>
