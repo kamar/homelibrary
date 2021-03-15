ff504262b359f1289ba5749012aaae01c24ab688
Konstas Marmatakis <marmako@gmail.com>
2021-03-15 15:17:20 +0200

    Some changes.

--- app/php/authordetails.php
+++ app/php/authordetails.php
@@ -1,10 +1,29 @@
 <?php
     session_start();
-    require_once '../../ics/head.php'
+    require_once '../../ics/head.php';
+    require 'connect.php';
+    $authorid = intval($_GET['aid']);
+    $query ="SELECT * FROM tbl_author WHERE author_id = :aid";
+    $con = pdo_homelibrary();
+    $result = $con->prepare($query);
+    $result->bindParam(':aid', $authorid, PDO::PARAM_INT);
+
+    $author = $result->execute();
+    if (sizeof($author)== 1){
+        foreach ($author as $au){
+            //  TODO: Formatting and php code in database. Button for writer's books.
+            echo '<div class="authordetail">';
+            echo '    <div class="btnbooksauthor">';
+            
+            echo '    </div>';
+            echo '    <div id="fullname">Fullname: '.$au['firstname'].' '.$au['surname'].'</div>';
+            echo '    <div>Email: '.'</div>';
+            echo '    <div>Internet: '.'</div>';
+            echo '    <div>Bio: '.'</div>';
+            echo '</div>';
+        }
+    } 
+    $con = null;
 ?>
-<!-- TODO: Formatting and php code in database. Button for writer's books. -->
-<div>
-    <div id="fullname">Fullname:<?php echo $_GET['name']; ?></div>
-</div>
 </body>
 </html>
