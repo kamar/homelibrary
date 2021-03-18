780ff5b9253f7bd9d7cc95b9dc7d30777c9ac982
Konstas Marmatakis <marmako@gmail.com>
2021-03-18 22:11:37 +0200

    Minor changes,

--- app/js/menu.js
+++ app/js/menu.js
@@ -1,15 +1,15 @@
 function testme() {
   var current = document.getElementsByClassName("active");
   if (current.length > 0) {
-    current[0].className = current[0].className.replace(" active", "");
+    current[0].className = current[0].className.remove("active");
   }
   // current[0].className = current[0].className.replace(" active", "");
-  this.className += " active";
+  this.className.add("active");
 }
 
-var btns = '';
-btns = document.getElementsByClassName("btn");
+var btns = [];
 // Get all buttons with class="btn" inside the container
+btns = document.getElementsByClassName("btn");
 
 // Loop through the buttons and add the active class to the current/clicked button
 for (var i = 0; i < btns.length; i++) {
