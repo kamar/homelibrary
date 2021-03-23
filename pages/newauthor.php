2c81417b9265091356ff11a0b25c00f11f9ef7ff
Konstas Marmatakis <marmako@gmail.com>
2021-03-23 19:34:22 +0200

    Tranlation's work.

--- pages/newauthor.php
+++ pages/newauthor.php
@@ -13,31 +13,31 @@
     
     ?>
     <div class="container">
-    <h2>Εισαγωγή Συγγραφέα</h2>
+    <h2><?php echo _('New Author'); ?></h2>
       <form name="newauthor" action="/dist/php/insert_author"  onsubmit="return val_form()" method="post">
         <div class="row">
-          <div class="col-25"><label for="firstname">Όνομα Συγγραφέα:</label></div>
-          <div class="col-45"><input type="text" id="firstname" name="firstname" placeholder="Καταχωρείστε το όνομα του συγγραφέα."></div>
+          <div class="col-25"><label for="firstname"><?php echo _('Author\'s Name');?>:</label></div>
+          <div class="col-45"><input type="text" id="firstname" name="firstname" placeholder="<?php echo _('Author\'s Name'); ?>""></div>
           <div class="col-30"><span class="form_error">* <?php $isbn_error; ?></span></div>
         </div>
         <div class="row">
-          <div class="col-25"><label for="surname">Επώνυμο Συγγραφέα:</label></div>
-          <div class="col-45"><input type="text" id="surname" name="surname" placeholder="Καταχωρείστε το επώνυμο του συγγραφέα."></div>
+          <div class="col-25"><label for="surname"><?php echo _('Author\'s Surname');?>:</label></div>
+          <div class="col-45"><input type="text" id="surname" name="surname" placeholder="<?php echo _('Author\'s Surname');?>"></div>
         </div>
         <div class="row">
-          <div class="col-25"><label for="email">Email Συγγραφέα:</label></div>
-          <div class="col-45"><input type="email" id="email" name="email" placeholder="Καταχωρείστε το email του συγγραφέα."></div>
+          <div class="col-25"><label for="email"><?php echo _('Author\'s Email');?>:</label></div>
+          <div class="col-45"><input type="email" id="email" name="email" placeholder="<?php echo _('Author\'s Email');?>"></div>
         </div>
         <div class="row">
-          <div class="col-25"><label for="site">Ιστοσελίδα Συγγραφέα:</label></div>
-          <div class="col-45"><input type="url" id="site" name="site" placeholder="Καταχωρείστε την ιστοσελίδα του συγγραφέα."></div>
+          <div class="col-25"><label for="site"><?php echo _('Author\'s Site');?>:</label></div>
+          <div class="col-45"><input type="url" id="site" name="site" placeholder="<?php echo _('Author\'s Site');?>"></div>
         </div>
         <div class="row">
-          <div class="col-25"><label for="bio">Βιογραφικό Συγγραφέα:</label></div>
-          <div class="col-45"><textarea  rows="5" cols="50" id="bio" name="bio" placeholder="Καταχωρείστε το βιογραφικό του συγγραφέα."></textarea></div>
+          <div class="col-25"><label for="bio"><?php echo _('Author\'s Resume');?>:</label></div>
+          <div class="col-45"><textarea  rows="5" cols="50" id="bio" name="bio" placeholder="<?php echo _('Author\'s resume');?>"></textarea></div>
         </div>
         <div class="row">
-          <input type="submit" value="Καταχώρηση">
+          <input type="submit">
         </div>
       </form>
     </div>
